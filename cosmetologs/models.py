f203d61d660a4c5164cd02a65f4500ec48d7fc2a
DKostyuk <dkostyuk@ukr.net>
2022-01-18 14:15:46 +0200

    to see after renaming the project

--- cosmetologs/models.py
+++ cosmetologs/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from addresses.models import Address
 from django.contrib.auth.models import User
 from django.db.models.signals import pre_save
-from b_project.current_user import get_current_user
+from shor.current_user import get_current_user
 from ckeditor.fields import RichTextField
 import openpyxl
 # from django.utils import timezone
