1ce3d21e366316f5a867f9c17e08667613b10b84
DKostyuk <dkostyuk@ukr.net>
2022-01-20 09:42:28 +0200

    before server installation on hyperhost.ua

--- shor/settings_prod1.py
+++ shor/settings_prod1.py
@@ -4,11 +4,11 @@ ALLOWED_HOSTS = ['*']	# we can input IP address of the server where we can run o
 #settings for db on server - standard construction
 DATABASES = {
     'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',	#psycopg2 - connector Django & Postgres, have to be installed on the server
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',	#psycopg2 - connector Django & Postgres, have to be installed on the server
         'NAME': 'h49085c_db_shor',
         'USER': 'h49085c_dk',
         'PASSWORD': 'Tropinka123!',
-            'HOST': 'localhost',
+        'HOST': 'localhost',
         'PORT': '',                      # Set to empty string for default.
     }
 }
