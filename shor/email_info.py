c9ab5f063e0acc981f6baf4d0c4a6dd039329229
DKostyuk <dkostyuk@ukr.net>
2022-02-15 08:43:20 +0200

    2022-02-12 include try for get()

---
+++ shor/email_info.py
@@ -0,0 +1,10 @@
+# for shor.com.ua
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'shor.com.ua'
+EMAIL_HOST_USER = 'office@shor.com.ua'
+EMAIL_HOST_PASSWORD = 'SMTatkos14550!'
+EMAIL_PORT = 587
+
+EMAIL_FROM = 'noreply@shor.com.ua'
+EMAIL_ADMIN = 'dkostyuk@ukr.net'
